e3d53ae5a0a3ddb0528ff788d3b0f2b5db0d4f8d
keisuke watanabe <kskwtnb93@gmail.com>
2023-12-30 19:29:32 +0900

    ♻️ 不要コード除去

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,13 @@ export const runtime = 'edge'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24 pb-48 max-sm:p-4 max-sm:pb-32">
-      <div className="mb-8 mt-0 max-sm:mb-6 max-sm:mt-6 ">
+      <div className="mb-8 mt-0 max-sm:mb-6 max-sm:mt-6">
         <h1 className="text-center text-4xl font-bold leading-normal max-sm:text-2xl">
           料金計算
         </h1>
       </div>
 
       <FeesCalculation />
-
-      {/* <Counter /> */}
     </main>
   )
 }
